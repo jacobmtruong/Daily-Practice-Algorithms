650b08d1cf6a436b2b3b19bb4bb944cf0277468c
Jacob Truong <jacob.truong@ymail.com>
2022-10-07 16:38:27 -0700

    daily algos

---
+++ Daily_Practice/Algebra/Number_split.js
@@ -0,0 +1,23 @@
+// Number Split
+// Given a number, return an array containing the two halves of the number. If the number is odd, make the rightmost number higher.
+
+// Examples
+// numberSplit(4) ➞ [2, 2]
+
+// numberSplit(10) ➞ [5, 5]
+
+// numberSplit(11) ➞ [5, 6]
+
+// numberSplit(-9) ➞ [-5, -4]
+// Notes
+// All numbers will be integers.
+// You can expect negative numbers too.
+
+
+function numberSplit(num) {
+    let number = Math.round(num / 2)
+    let arr = [num - number, number]
+    return arr
+}
+
+console.log(numberSplit(-11));

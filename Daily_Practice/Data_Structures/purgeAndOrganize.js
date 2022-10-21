74e3189a65a55efa9caee43ce7a283499f7eef70
Jacob Truong <jacob.truong@ymail.com>
2022-10-21 10:28:49 -0700

    hashmap solution

--- Daily_Practice/Data_Structures/purgeAndOrganize.js
+++ Daily_Practice/Data_Structures/purgeAndOrganize.js
@@ -8,4 +8,25 @@ function purgeAndOrganize(arr) {
     return newArray
 }
 
+
+// hashMap
+function purgeAndOrganizeMap (arr) {
+    let obj = {}
+    for (let i = 0; i < arr.length; i++) {
+        if(!obj[arr[i]]) {
+            obj[arr[i]] = 1
+        }
+    }
+    let newArray = Object.keys(obj).sort((a,b) => a - b)
+    let arr1 = []
+    for (let i = 0; i < newArray.length; i++) {
+        arr1.push(parseInt(newArray[i]))
+    }
+    return arr1
+}
+
+
+
 console.log(purgeAndOrganize([1, 4, 4, 4, 4, 4, 3, 2, 1, 2]));
+
+console.log(purgeAndOrganizeMap([1, 4, 4, 4, 4, 4, 3, 2, 1, 2]));

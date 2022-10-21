442dde1ec1f1dba1d74df9af04a19df553dc76b5
Jacob Truong <jacob.truong@ymail.com>
2022-10-20 21:18:17 -0700

    daily practice

--- Technical-questions/Linked-lists/practiceFile.js
+++ Technical-questions/Linked-lists/practiceFile.js
@@ -11,15 +11,53 @@ class LinkedList {
         this.size = 0
     }
 
+    addAtIndex(data, index) {
+        if (index === 0) {
+            this.head = new Node(data, this.head);
+            return
+        } 
+        if (index > 0 && index > this.size) {
+            return "Invalid index"
+        }
+         const node = new Node(data)
+         let current,previous 
+
+         current = this.head
+         let count = 0
+         
+         while (count < index) {
+            previous = current
+            count++
+            current = current.next
+         }
+
+         node.next = current
+         previous.next = node
+         this.size++
+    }
 
     addToFront(data) {
-        this.head = new Node(data, this.next);
+        this.head = new Node(data, this.head);
         this.size++
     }
+
+    printList() {
+        let current = this.head
+        while (current) {
+            console.log(current.data);
+            current = current.next
+        }
+    }
+
 }
 
 let linkedList = new LinkedList
 
 linkedList.addToFront(20)
+linkedList.addToFront(10)
+linkedList.addToFront(5)
+linkedList.addAtIndex(2, 1)
+
+linkedList.printList()
 
 // LinkedList { head: Node { data: 20, next: null }, size: 1 }

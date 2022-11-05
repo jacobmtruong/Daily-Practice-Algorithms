54f20758d765cb7b30ee166a21433c4f3c64e451
Jacob Truong <jacob.truong@ymail.com>
2022-11-05 12:22:23 -0700

    On2

--- Technical-questions/Array-and-Strings/containsDuplicate.js
+++ Technical-questions/Array-and-Strings/containsDuplicate.js
@@ -18,6 +18,23 @@ const containsDuplicate = nums => {
     }
 }
 
+
+const containsDuplicate2 = nums => {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+
 console.log(containsDuplicate([1,2,3,1]));
 console.log(containsDuplicate([1,2,3,4]));
 console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]));
+
+console.log(containsDuplicate2([1,2,3,1]));
+console.log(containsDuplicate2([1,2,3,4]));
+console.log(containsDuplicate2([1,1,1,3,3,4,3,2,4,2]));

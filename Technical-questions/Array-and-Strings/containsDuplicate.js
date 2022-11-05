b0d33768197ea27ae1ab9bb9bb065a78586a8879
Jacob Truong <jacob.truong@ymail.com>
2022-11-05 12:09:58 -0700

    algos

---
+++ Technical-questions/Array-and-Strings/containsDuplicate.js
@@ -0,0 +1,23 @@
+// Example 1:
+// Input: nums = [1,2,3,1]
+// Output: true
+
+// Example 2:
+// Input: nums = [1,2,3,4]
+// Output: false
+
+// Example 3:
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+
+const containsDuplicate = nums => {
+    let arr = nums.sort();
+    for (let i = 0; i < arr.length; i++) {
+        return arr[i] === arr[i + 1] 
+    }
+}
+
+console.log(containsDuplicate([1,2,3,1]));
+console.log(containsDuplicate([1,2,3,4]));
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]));
--- Technical-questions/Linked-lists/practiceFile.js
+++ Technical-questions/Linked-lists/practiceFile.js
@@ -1,97 +1,34 @@
 class Node {
-    constructor(data, next = null) {
-        this.data = data;
-        this.next = next;
-    }
+    constructor (data, next = null) {
+        this.data = data
+        this.next = next
+    } 
 }
 
 class LinkedList {
-    constructor() {
+    constructor () {
         this.head = null;
         this.size = 0
     }
 
-    addAtIndex(data, index) {
-        if (index === 0) {
-            this.head = new Node(data, this.head);
-            return
-        } 
-        if (index > 0 && index > this.size) {
-            return "Invalid index"
-        }
-         const node = new Node(data)
-         let current,previous 
-
-         current = this.head
-         let count = 0
-         
-         while (count < index) {
-            previous = current
-            count++
-            current = current.next
-         }
-
-         node.next = current
-         previous.next = node
-         this.size++
-    }
-
-    addToFront(data) {
-        this.head = new Node(data, this.head);
-        this.size++
-    }
-
-    addtoBack(data) {
-        if (this.head == null) {
-            this.head = new Node(data, this.head);
-        } else {
-            const node = new Node(data)
-            let current = this.head
-
-            while (current.next != null) {
-                current = current.next
-            }
-
-            current.next = node
-            this.size++
-        }
+    addToFront (data) {
+        this.head = new Node (data, this.head)
+        this.size ++
     }
 
     printList() {
-        let current = this.head
-        while (current) {
-            console.log(current.data);
-            current = current.next
-        }
-    }
+        let runner = this.head
 
-    removeDups() {
-        let current = this.head
-        while (current !== null && current.next !== null) {
-            if (current.data === current.next.data) {
-                current.next = current.next.next
-            } else {
-                current = current.next
-            }
+        while (runner) {
+            console.log(runner.data);
+            runner = runner.next
         }
-        return console.log(this.head);
+    console.log(this.size); 
     }
-
 }
 
-let linkedList = new LinkedList
+let linkedlist = new LinkedList()
 
-linkedList.addToFront(20)
-linkedList.addToFront(10)
-linkedList.addToFront(10)
-linkedList.addToFront(5)
-linkedList.addToFront(5)
-linkedList.addtoBack(50)
-linkedList.addtoBack(50)
+linkedlist.addToFront(20)
 
-// linkedList.printList()
-linkedList.removeDups()
-
-linkedList.printList()
-
-// LinkedList { head: Node { data: 20, next: null }, size: 1 }
+linkedlist.printList()

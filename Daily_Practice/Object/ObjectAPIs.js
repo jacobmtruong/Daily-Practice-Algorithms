3a93d18d290338bb7b3082b7c7df1256f6f8284d
Jacob Truong <jacob.truong@ymail.com>
2022-12-02 19:39:03 -0800

    practice

--- Daily_Practice/Object/ObjectAPIs.js
+++ Daily_Practice/Object/ObjectAPIs.js
@@ -16,7 +16,7 @@ const foods = [
     }
 ]
 
-const getTotal = (object) => {
+const getPrices = (object) => {
     let prices = []
     for (let i = 0; i < object.length; i++) {
         prices.push(foods[i].price)
@@ -24,6 +24,17 @@ const getTotal = (object) => {
     return prices
 }
 
-console.log(getTotal(foods));
+console.log(getPrices(foods));
 // [3,4,2]
 
+
+const getTotalPrice = (foods) => {
+    let total = 0
+    for (let i = 0; i < foods.length; i++) {
+        total += foods[i].price
+    }
+    return total
+}
+
+console.log(getTotalPrice(foods));
+// 10

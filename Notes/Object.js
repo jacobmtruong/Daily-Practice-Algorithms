ce05f7e9deab3c2b7ac81f2d00c3834bd507a74e
Jacob Truong <jacob.truong@ymail.com>
2022-12-03 10:31:36 -0800

    note

--- Notes/Object.js
+++ Notes/Object.js
@@ -13,6 +13,45 @@ Object.getOwnPropertyNames(object1) // [a, b]
 
 
 
+const person = {
+    name: 'Dom'
+}
+
+// ============ .assign() ============
+const newPerson = Object.assign({}, person, {
+    name: 'Jeff',
+    age: 29
+})
+
+// Also we can use spread operator fro modern code
+
+const newPerson1 = {...person, ... {
+    name: 'John',
+    age: 24
+}}
+ 
+//console.log(newPerson);   //{ name: 'Jeff', age: 29 }
+//console.log(person);      // { name: 'Dom' }
+//console.log(newPerson1);  // { name: 'John', age: 24 }
+
+
+
+// ============ .defineProperty() ============
+const person1 = {}
+
+Object.defineProperty(person1, "date", {
+    get() {
+        return new Date();
+    }
+})
+
+
+Object.defineProperty(person1, "name", {
+    value: 'John'
+})
+
+console.log(person1.date);  //2022-12-03T18:28:55.074Z
+console.log(person1.name);  //John
 
 
 

3730a82701d0c0effac8457c6c8f9642371e41af
Jacob Truong <jacob.truong@ymail.com>
2022-12-03 16:27:51 -0800

    notes

--- Notes/Object.js
+++ Notes/Object.js
@@ -87,15 +87,23 @@ delete person2.age
 //console.log(person2)    //{ name: 'John', age: 34 }
 
 
+// =============== MUNIPULATION =================
+
 var dog = {name: 'Mika', type: 'dog'}
 var cat = {name: 'Frida', type: 'cat'}
 
 var pets = [dog, cat]
-//console.log(pets);    //[ { name: 'Mika', type: 'dog' }, { name: 'Frida', type: 'cat' } ]
+//console.log(pets);            //[ { name: 'Mika', type: 'dog' }, { name: 'Frida', type: 'cat' } ]
 
-cat.age = 10 
-pets[0].age = 5
+cat.age = 10                    // { name: 'Frida', type: 'cat', age: 10 }
+pets[0].age = 5                 // { name: 'Mika', type: 'dog', age: 5 }
+pets[1]['naughty'] = true       // { name: 'Frida', type: 'cat', age: 10, naughty: true }
 
-//console.log(pets);    //[{ name: 'Mika', type: 'dog' },{ name: 'Frida', type: 'cat', age: 10 }]
+
+console.log(pets);
+/* [
+  { name: 'Mika', type: 'dog', age: 5 },
+  { name: 'Frida', type: 'cat', age: 10, naughty: true }
+] */
 
 

c054982eb2abd0c605e6387297f40c3be69f35f6
Jacob Truong <jacob.truong@ymail.com>
2022-12-29 20:58:49 -0800

    twosum

---
+++ LeetCodeChallenges/Array&Hashing/TwoSum.js
@@ -0,0 +1,17 @@
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+const twoSum = (nums, target) => {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [nums[i], nums[j]];
+      }
+    }
+  }
+  return "There is no twosum in this array";
+};
+
+console.log(twoSum([2, 4, 7, 15], 9));
+console.log(twoSum([2, 3, 4], 6));

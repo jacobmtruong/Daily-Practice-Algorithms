b0b242b7d7506c87b5e01e9a687d853f3a2021f8
Jacob Truong <jacob.truong@ymail.com>
2023-02-23 07:39:21 -0800

    data landscape

--- Daily_Practice/Object/PeopleBudget.js
+++ Daily_Practice/Object/PeopleBudget.js
@@ -1,15 +1,26 @@
 let people = [
-    { name: "John", age: 21, budget: 23000 },
-    { name: "Steve",  age: 32, budget: 40000 },
-    { name: "Martin",  age: 16, budget: 2700 }
-]
+  { name: "John", age: 21, budget: 23000 },
+  { name: "Steve", age: 32, budget: 40000 },
+  { name: "Martin", age: 16, budget: 2700 },
+];
+let things = {
+  food: {
+    landscape: [
+      { name: "John", age: 21, budget: 23000 },
+      { name: "Steve", age: 32, budget: 40000 },
+      { name: "Martin", age: 16, budget: 2700 },
+    ],
+  },
+};
 
 const getBudget = (list) => {
-    let total = 0
-    for (let i = 0; i < list.length; i++) {
-        total += list[i].budget
-    }
-    return total
-}
+  let total = 0;
+  for (let i = 0; i < list.length; i++) {
+    total += list[i].budget;
+  }
+  return total;
+};
 
 console.log(getBudget(people));
+
+console.log(things.food.landscape);

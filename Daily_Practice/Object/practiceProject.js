9d21cc08275f93727d2cefc21294345c3205f18e
Jacob Truong <jacob.truong@ymail.com>
2023-03-02 22:20:16 -0800

    commit

--- Daily_Practice/Object/practiceProject.js
+++ Daily_Practice/Object/practiceProject.js
@@ -1,164 +1,135 @@
-const photography = [
+const images = [
   {
-    food: {
-      landscape: [
-        {
-          id: 1,
-          url: "https://live.staticflickr.com/65535/52693970615_34efb6d5fd_k.jpg",
-          content: "Food Photography",
-          favorite: false,
-        },
-        {
-          id: 2,
-          url: "https://live.staticflickr.com/65535/52693825269_9cf72f651b_k.jpg",
-          content: "Food Photography",
-          favorite: false,
-        },
-        {
-          id: 3,
-          url: "https://live.staticflickr.com/65535/52693560371_fea8584439_k.jpg",
-          content: "Food Photography",
-          favorite: false,
-        },
-        {
-          id: 4,
-          url: "https://live.staticflickr.com/65535/52693970345_69a63631be_k.jpg",
-          content: "Food Photography",
-          favorite: false,
-        },
-        {
-          id: 5,
-          url: "https://live.staticflickr.com/65535/52694050803_594338d08a_k.jpg",
-          content: "Food Photography",
-          favorite: true,
-        },
+    id: 1,
+    url: "https://live.staticflickr.com/65535/52693970615_34efb6d5fd_k.jpg",
+    content: "Food Photography",
+    favorite: false,
+    category: "food",
+    view: "landscape",
+  },
+  {
+    id: 2,
+    url: "https://live.staticflickr.com/65535/52693825269_9cf72f651b_k.jpg",
+    content: "Food Photography",
+    favorite: false,
+    category: "food",
+    view: "landscape",
+  },
+  {
+    id: 3,
+    url: "https://live.staticflickr.com/65535/52693560371_fea8584439_k.jpg",
+    content: "Food Photography",
+    favorite: false,
+    category: "food",
+    view: "landscape",
+  },
+  {
+    id: 4,
+    url: "https://live.staticflickr.com/65535/52693970345_69a63631be_k.jpg",
+    content: "Food Photography",
+    favorite: false,
+    category: "food",
+    view: "landscape",
+  },
+  {
+    id: 5,
+    url: "https://live.staticflickr.com/65535/52694050803_594338d08a_k.jpg",
+    content: "Food Photography",
+    favorite: true,
+    category: "food",
+    view: "landscape",
+  },
 
-        {
-          id: 6,
-          url: "https://live.staticflickr.com/65535/52694043635_95691a8b85_b.jpg",
-          content: "Food Photography",
-          favorite: false,
-        },
-      ],
-      portrait: [
-        {
-          id: 1,
-          url: "https://live.staticflickr.com/65535/52693627716_148a638a3e_h.jpg",
-          content: "Food Photography",
-          favorite: false,
-        },
-        {
-          id: 2,
-          url: "https://live.staticflickr.com/65535/52694041290_10ee535607_b.jpg",
-          content: "Food Photography",
-          favorite: false,
-        },
-        {
-          id: 3,
-          url: "https://live.staticflickr.com/65535/52694119243_e3216085cf_b.jpg",
-          content: "Food Photography",
-          favorite: false,
-        },
-      ],
-    },
+  {
+    id: 6,
+    url: "https://live.staticflickr.com/65535/52694043635_95691a8b85_b.jpg",
+    content: "Food Photography",
+    favorite: false,
+    category: "food",
+    view: "landscape",
   },
   {
-    beverage: {
-      landscape: [null],
-      portrait: [
-        {
-          id: 1,
-          url: "https://live.staticflickr.com/65535/52694047990_1fa6b74637_h.jpg",
-          content: "Beverage Photography",
-          favorite: false,
-        },
-        {
-          id: 2,
-          url: "https://live.staticflickr.com/65535/52694048690_6cc8f3b4e3_h.jpg",
-          content: "Beverage Photography",
-          favorite: false,
-        },
-        {
-          id: 3,
-          url: "https://live.staticflickr.com/65535/52694128513_846a95eaa5_b.jpg",
-          content: "Beverage Photography",
-          favorite: false,
-        },
-        {
-          id: 4,
-          url: "https://live.staticflickr.com/65535/52693119702_7c48febda6_h.jpg",
-          content: "Beverage Photography",
-          favorite: false,
-        },
-        {
-          id: 5,
-          url: "https://live.staticflickr.com/65535/52693672866_2841c2d5ca_h.jpg",
-          content: "Beverage Photography",
-          favorite: false,
-        },
-        {
-          id: 6,
-          url: "https://live.staticflickr.com/65535/52693938494_ecbb3e6b6b_h.jpg",
-          content: "Beverage Photography",
-          favorite: false,
-        },
-      ],
-    },
+    id: 7,
+    url: "https://live.staticflickr.com/65535/52693627716_148a638a3e_h.jpg",
+    content: "Food Photography",
+    favorite: false,
+    category: "food",
+    view: "portrait",
   },
   {
-    product: {
-      landscape: [
-        {
-          id: 1,
-          url: "https://live.staticflickr.com/65535/52694167823_2a81c9d614_h.jpg",
-          content: "Product Photography",
-          favorite: false,
-        },
-        {
-          id: 2,
-          url: "https://live.staticflickr.com/65535/52693161132_02435cb3e7_h.jpg",
-          content: "Product Photography",
-          favorite: false,
-        },
-        {
-          id: 3,
-          url: "https://live.staticflickr.com/65535/52694092260_a20d1b1a4a_h.jpg",
-          content: "Product Photography",
-          favorite: false,
-        },
-        {
-          id: 4,
-          url: "https://live.staticflickr.com/65535/52694172213_a5e9cce3e2_h.jpg",
-          content: "Product Photography",
-          favorite: false,
-        },
-      ],
-      portrait: [
-        {
-          id: 1,
-          url: "https://live.staticflickr.com/65535/52694174453_8cb155b6f7_h.jpg",
-          content: "Product Photography",
-          favorite: false,
-        },
-        {
-          id: 2,
-          url: "https://live.staticflickr.com/65535/52694185128_9207b10aa5_h.jpg",
-          content: "Product Photography",
-          favorite: false,
-        },
-      ],
-    },
+    id: 8,
+    url: "https://live.staticflickr.com/65535/52694041290_10ee535607_b.jpg",
+    content: "Food Photography",
+    favorite: false,
+    category: "food",
+    view: "portrait",
+  },
+  {
+    id: 9,
+    url: "https://live.staticflickr.com/65535/52694119243_e3216085cf_b.jpg",
+    content: "Food Photography",
+    favorite: false,
+    category: "food",
+    view: "portrait",
+  },
+  {
+    id: 1,
+    url: "https://live.staticflickr.com/65535/52694047990_1fa6b74637_h.jpg",
+    content: "Beverage Photography",
+    favorite: false,
+    category: "beverage",
+    view: "portrait",
+  },
+  {
+    id: 2,
+    url: "https://live.staticflickr.com/65535/52694048690_6cc8f3b4e3_h.jpg",
+    content: "Beverage Photography",
+    favorite: false,
+    category: "beverage",
+    view: "portrait",
+  },
+  {
+    id: 3,
+    url: "https://live.staticflickr.com/65535/52694128513_846a95eaa5_b.jpg",
+    content: "Beverage Photography",
+    favorite: false,
+    category: "beverage",
+    view: "portrait",
+  },
+  {
+    id: 4,
+    url: "https://live.staticflickr.com/65535/52693119702_7c48febda6_h.jpg",
+    content: "Beverage Photography",
+    favorite: false,
+    category: "beverage",
+    view: "portrait",
+  },
+  {
+    id: 5,
+    url: "https://live.staticflickr.com/65535/52693672866_2841c2d5ca_h.jpg",
+    content: "Beverage Photography",
+    favorite: false,
+    category: "beverage",
+    view: "portrait",
+  },
+  {
+    id: 6,
+    url: "https://live.staticflickr.com/65535/52693938494_ecbb3e6b6b_h.jpg",
+    content: "Beverage Photography",
+    favorite: false,
+    category: "beverage",
+    view: "portrait",
   },
 ];
 
-// function random(pics) {
-//   pics.forEach((category) => {
-//     console.log(Object.values(category)[0].landscape[1]);
-//     let array = [];
-//     array.push(Object.values(category)[0].landscape);
+function filterFood(data) {
+  return data.filter(function (item) {
+    return item.category === "food";
+  });
+}
 
-//     return array;
-//   });
-// }
+function shuffle(data) {
+  return data.sort((a, b) => 0.5 - Math.random());
+}
 
-console.log(photography);
+console.log(shuffle(images));
---
+++ Notes/Array-Functions/worksheet.js
@@ -0,0 +1,72 @@
+const characters = [
+  {
+    name: "Luke Skywalker",
+    height: "172",
+    mass: "77",
+    hair_color: "blond",
+    skin_color: "fair",
+    eye_color: "blue",
+    birth_year: "19BBY",
+    gender: "male",
+  },
+  {
+    name: "Darth Vader",
+    height: "202",
+    mass: "136",
+    hair_color: "none",
+    skin_color: "white",
+    eye_color: "yellow",
+    birth_year: "41.9BBY",
+    gender: "male",
+  },
+  {
+    name: "Leia Organa",
+    height: "150",
+    mass: "49",
+    hair_color: "brown",
+    skin_color: "light",
+    eye_color: "brown",
+    birth_year: "19BBY",
+    gender: "female",
+  },
+  {
+    name: "Owen Lars",
+    height: "178",
+    mass: "120",
+    hair_color: "brown, grey",
+    skin_color: "light",
+    eye_color: "blue",
+    birth_year: "52BBY",
+    gender: "male",
+  },
+  {
+    name: "Beru Whitesun lars",
+    height: "165",
+    mass: "75",
+    hair_color: "brown",
+    skin_color: "light",
+    eye_color: "blue",
+    birth_year: "47BBY",
+    gender: "female",
+  },
+];
+
+// *** FILTER ***
+// 1. Get all characters with mass greater than 100
+const massGreater100 = characters.filter((character) => character.mass > 100);
+// console.log(massGreater100);
+// 2. Get all character with height greater than 200
+const heightGreater200 = characters.filter(
+  (character) => character.height > 200
+);
+// console.log(heightGreater200);
+// 3. Get all the male characters
+const maleCharacters = characters.filter(
+  (character) => character.gender === "male"
+);
+// console.log(maleCharacters);
+// 4. Get all the female characters
+const femaleCharacters = characters.filter(
+  (character) => character.gender === "female"
+);
+console.log(femaleCharacters);

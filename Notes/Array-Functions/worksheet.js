7861ec17ab4c8f3203933a95b62c2f31d015cd6c
Jacob Truong <jacob.truong@ymail.com>
2023-03-06 13:35:21 -0800

    commit

--- Notes/Array-Functions/worksheet.js
+++ Notes/Array-Functions/worksheet.js
@@ -55,16 +55,19 @@ const characters = [
 // 1. Get all characters with mass greater than 100
 const massGreater100 = characters.filter((character) => character.mass > 100);
 // console.log(massGreater100);
+
 // 2. Get all character with height greater than 200
 const heightGreater200 = characters.filter(
   (character) => character.height > 200
 );
 // console.log(heightGreater200);
+
 // 3. Get all the male characters
 const maleCharacters = characters.filter(
   (character) => character.gender === "male"
 );
 // console.log(maleCharacters);
+
 // 4. Get all the female characters
 const femaleCharacters = characters.filter(
   (character) => character.gender === "female"
@@ -75,15 +78,35 @@ console.log(femaleCharacters);
 // 1. Get array pf all the names
 const names = characters.map((character) => character.name);
 // console.log(names);
+
 // 2. Get array of all the heights
 const heights = characters.map((character) => character.height);
 // console.log(heights);
+
 // 3. Get array of all objects included name and height
 const namesandheight = characters.map((character) => ({
   name: character.name,
   height: character.height,
 }));
 // console.log(namesandheight);
+
 // 4. Get array of all first names
 const firstNames = characters.map((character) => character.name.split(" ")[0]);
 console.log(firstNames);
+
+// *** SOME ***
+// 1. At least one male character
+const oneMale = characters.some((character) => character.gender === "male");
+// console.log(oneMale);
+
+// 2. At least one character with blue eye
+const blueEye = characters.some((character) => character.eye_color === "blue");
+// console.log(blueEye);
+
+// 3. At least one character has a height taller than 200
+const taller200 = characters.some((character) => character.height > 200);
+// console.log(taller200);
+
+// 4. At least one character has a mass less than 50
+const massless50 = characters.some((character) => character.mass < 50);
+console.log(massless50);
